2c65a0bf94ea2e115f5622f014bc60433588b6f1
daveg7lee <daveg7lee@gmail.com>
2023-03-14 20:49:23 +0900

    🐛 빌드 버그 수정

--- src/components/home/Swiper.tsx
+++ src/components/home/Swiper.tsx
@@ -14,16 +14,16 @@ export default function HomeSwiper() {
       pagination={{ clickable: true }}
     >
       <SwiperSlide>
-        <Center h="70vh">Slide 1</Center>
+        <Center h="80vh">Slide 1</Center>
       </SwiperSlide>
       <SwiperSlide>
-        <Center h="70vh">Slide 2</Center>
+        <Center h="80vh">Slide 2</Center>
       </SwiperSlide>
       <SwiperSlide>
-        <Center h="70vh">Slide 3</Center>
+        <Center h="80vh">Slide 3</Center>
       </SwiperSlide>
       <SwiperSlide>
-        <Center h="70vh">Slide 4</Center>
+        <Center h="80vh">Slide 4</Center>
       </SwiperSlide>
     </Swiper>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,38 @@
 import HomeSwiper from "@/components/home/Swiper";
-import { Box, Grid, GridItem, Text } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Link, Text } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <Box minH="90vh" pb={20}>
       <HomeSwiper />
-      <Text textAlign="center" my={5} fontSize="3xl" fontWeight="extrabold">
-        인기 반찬
-      </Text>
-      <Grid
-        templateColumns="repeat(4, 1fr)"
-        gap={6}
-        px={{ sm: "5", md: "14", lg: "36" }}
-      >
-        <GridItem w="300" h="300" bg="blue.500" />
-        <GridItem w="300" h="300" bg="blue.500" />
-        <GridItem w="300" h="300" bg="blue.500" />
-        <GridItem w="300" h="300" bg="blue.500" />
-        <GridItem w="300" h="300" bg="blue.500" />
-        <GridItem w="300" h="300" bg="blue.500" />
-        <GridItem w="300" h="300" bg="blue.500" />
-        <GridItem w="300" h="300" bg="blue.500" />
-        <GridItem w="300" h="300" bg="blue.500" />
-        <GridItem w="300" h="300" bg="blue.500" />
-        <GridItem w="300" h="300" bg="blue.500" />
-        <GridItem w="300" h="300" bg="blue.500" />
-      </Grid>
+      <Box px={{ sm: "5", md: "14", lg: "36" }}>
+        <Box
+          mt={10}
+          mb={5}
+          display="flex"
+          alignItems="end"
+          justifyContent="space-between"
+        >
+          <Text fontSize="3xl" fontWeight="extrabold">
+            인기 반찬
+          </Text>
+          <Link>더보기</Link>
+        </Box>
+        <Grid templateColumns="repeat(4, 1fr)" gap={6}>
+          <GridItem w="300" h="300" bg="blue.500" />
+          <GridItem w="300" h="300" bg="blue.500" />
+          <GridItem w="300" h="300" bg="blue.500" />
+          <GridItem w="300" h="300" bg="blue.500" />
+          <GridItem w="300" h="300" bg="blue.500" />
+          <GridItem w="300" h="300" bg="blue.500" />
+          <GridItem w="300" h="300" bg="blue.500" />
+          <GridItem w="300" h="300" bg="blue.500" />
+          <GridItem w="300" h="300" bg="blue.500" />
+          <GridItem w="300" h="300" bg="blue.500" />
+          <GridItem w="300" h="300" bg="blue.500" />
+          <GridItem w="300" h="300" bg="blue.500" />
+        </Grid>
+      </Box>
     </Box>
   );
 }
